3d6f40d950f8e9243ee011841614c96cd7e16206
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-09-13 21:56:58 +0700

    add signup vc

---
+++ Smack/Controller/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  Smack
+//
+//  Created by Patcharapon Joksamut on 13/9/2560 BE.
+//  Copyright © 2560 Patcharapon Joksamut. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
